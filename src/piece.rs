ab0615873a80d48b45379d62b1e7cde0e2cc8100
moonyango <mosesadrian825@gmail.com>
2025-09-09 04:18:14 +0300

    feat: add constructior method for piece object

--- src/piece.rs
+++ src/piece.rs
@@ -3,3 +3,13 @@ pub struct Piece {
     pub height: usize,
     pub pattern: Vec<Vec<char>>,
 }
+
+impl Piece {
+    pub fn new(width: usize, height: usize, pattern: Vec<Vec<char>>) -> Self {
+        Self {
+            width,
+            height,
+            pattern,
+        }
+    }
+}
