504bd0b463040146c12cd030bed1f11ae0da5a4b
moonyango <mosesadrian825@gmail.com>
2025-09-09 15:37:50 +0300

    feat: fetch player number and create the game object

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,15 @@ mod filler_ai;
 mod player;
 
 fn main() {
-    println!("Hello, world!");
+
+    std::fs::write("game_input.log", "").ok();
+    std::fs::write("ai_decisions.log", "").ok();
+    
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    if let Some(Ok(player_line)) = lines.next() {
+        let player_number = if player_line.contains("p1") { 1 } else { 2 };
+        let mut ai = FillerAi::new(player_number);
+    }
 }
