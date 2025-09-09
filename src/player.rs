974a044d0606f1aa6337712dd2b601c6a27abe82
moonyango <mosesadrian825@gmail.com>
2025-09-09 04:18:03 +0300

    feat: add constructior method for player object

--- src/player.rs
+++ src/player.rs
@@ -3,3 +3,22 @@ pub struct Player {
     pub territory_symbol: char,    // '@' or '$'
     pub last_placed_symbol: char,  // 'a' or 's'
 }
+
+impl Player {
+    pub fn new(number: u8) -> Self{
+        if number == 1 {
+            Self{ 
+                number: 1,
+                territory_symbol: '@',
+                last_placed_symbol: 'a',
+            }
+        } else {
+            Self{ 
+                number: 2,
+                territory_symbol: '$',
+                last_placed_symbol: 's',
+            }
+        }
+    }
+    
+}
